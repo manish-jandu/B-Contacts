5d553a9c68f309032c50fabe2cbffdc8b951b1b3
Manish <manishjandu1@gmail.com>
2021-06-19 14:51:33 +0530

    back button functionality added

--- app/src/main/java/com/manishjandu/bcontacts/ui/MainActivity.kt
+++ app/src/main/java/com/manishjandu/bcontacts/ui/MainActivity.kt
@@ -1,29 +1,68 @@
 package com.manishjandu.bcontacts.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.manishjandu.bcontacts.R
 import com.manishjandu.bcontacts.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var _binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding = ActivityMainBinding.inflate(layoutInflater)
+        _binding=ActivityMainBinding.inflate(layoutInflater)
+        bottomNavigationView=_binding.bottomBar
         setContentView(_binding.root)
 
-        val navHostFragment =
+        val navHostFragment=
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        val navController=navHostFragment.navController
 
-        val appBarConfiguration = AppBarConfiguration(setOf(R.id.BContactFragment,R.id.AllContactFragment))
-        setupActionBarWithNavController(navController,appBarConfiguration)
+        bottomNavigationView.setupWithNavController(navController)
 
-        _binding.bottomBar.setupWithNavController(navController)
+        appBarConfiguration=
+            AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.AllContactFragment || destination.id == R.id.BContactFragment) {
+                bottomNavigationView.visibility=View.VISIBLE
+            } else {
+                bottomNavigationView.visibility=View.GONE
+            }
+        }
+
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment=
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController=navHostFragment.navController
+
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onBackPressed() {
+        val navHostFragment=
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController=navHostFragment.navController
+
+        if (navController.graph.startDestination == navController.currentDestination?.id) {
+            super.onBackPressed()
+        } else {
+            navController.navigateUp(appBarConfiguration)
+        }
+
+    }
+
 }
+
