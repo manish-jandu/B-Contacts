e3f130ffbc80aa65cac3e523313ac8077f39fb7a
Manish <manishjandu1@gmail.com>
2021-06-20 19:09:16 +0530

    messages permission added

--- app/src/main/java/com/manishjandu/bcontacts/ui/fragments/addEditMessage/AddEditMessageViewModel.kt
+++ app/src/main/java/com/manishjandu/bcontacts/ui/fragments/addEditMessage/AddEditMessageViewModel.kt
@@ -1,21 +1,37 @@
 package com.manishjandu.bcontacts.ui.fragments.addEditMessage
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.telephony.SmsManager
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 
-class AddEditMessageViewModel : ViewModel() {
+class AddEditMessageViewModel(val app:Application) : AndroidViewModel(app) {
 
     fun setFutureMessage(
-        minutes: Int, hour: Int, day: Int, month: Int, year: Int, timeInMills: Long,
+        minutes: Int, hour: Int, day: Int, month: Int, year: Int, timeInMillis: Long,
         message: String, contactId: Long, contactNumber: String, messageId: Int
     )=viewModelScope.launch {
-        if(messageId == 0){
-            //Todo: new message
-        }else{
-            //Todo: edited Message
-        }
+//        if(messageId == 0){
+//            val alarmManager = getSystemService(app.applicationContext.ALARM_SERVICE) as AlarmManager
+//            val intent = Intent(app.applicationContext, FutureMessage::class.java)
+//            val pendingIntent = PendingIntent.getBroadcast(app.applicationContext, 0, intent, 0)
+//            alarmManager.setRepeating(
+//                AlarmManager.RTC,
+//                timeInMillis,
+//                AlarmManager.INTERVAL_DAY,
+//                pendingIntent
+//            )
+//            Toast.makeText(this, "Alarm is set", Toast.LENGTH_SHORT).show()
+//        }else{
+//            //Todo: edited Message
+//        }
+
     }
 
+    private fun sendSms(){
+        SmsManager.getDefault().sendTextMessage("addresswith+91", null,
+            "this is trial message", null, null);
+    }
 
 }
--- app/src/main/java/com/manishjandu/bcontacts/ui/fragments/messages/MessagesFragment.kt
+++ app/src/main/java/com/manishjandu/bcontacts/ui/fragments/messages/MessagesFragment.kt
@@ -1,25 +1,43 @@
 package com.manishjandu.bcontacts.ui.fragments.messages
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.afollestad.assent.*
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 import com.manishjandu.bcontacts.R
 import com.manishjandu.bcontacts.databinding.FragmentMessagesBinding
 
 class MessagesFragment : Fragment(R.layout.fragment_messages) {
     private lateinit var binding: FragmentMessagesBinding
+    private lateinit var floatingButtonAddMessage:FloatingActionButton
     private val arguments: MessagesFragmentArgs by navArgs()
+
+    override fun onStart() {
+        super.onStart()
+        if(!checkSmsPermission()){
+            floatingButtonAddMessage.visibility = View.GONE
+            setSmsPermission()
+        }else{
+            floatingButtonAddMessage.visibility = View.VISIBLE
+            //Todo:get messages
+        }
+
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding=FragmentMessagesBinding.bind(view)
+        floatingButtonAddMessage = binding.floatingButtonAddMessage
 
         val contactId=arguments.contactId
         val contactNumber=arguments.phone
 
-        binding.floatingButtonAddMessage.setOnClickListener {
+        floatingButtonAddMessage.setOnClickListener {
             val action=MessagesFragmentDirections.actionMessagesFragmentToAddEditMessageFragment(
                 contactId,
                 contactNumber,
@@ -28,4 +46,47 @@ class MessagesFragment : Fragment(R.layout.fragment_messages) {
             findNavController().navigate(action)
         }
     }
+
+    private fun checkSmsPermission(): Boolean {
+        return isAllGranted(Permission.SEND_SMS)
+    }
+
+    private fun setSmsPermission() {
+        askForPermissions(
+            Permission.SEND_SMS,
+        ) { result ->
+
+            if (result.isAllGranted()) {
+                floatingButtonAddMessage.visibility = View.VISIBLE
+                //Todo:get messages
+            }
+
+            if (result[Permission.SEND_SMS] == GrantResult.DENIED
+            ) {
+                showSnackBarOnPermissionError("Permission is required", "Ok") {
+                    setSmsPermission()
+                }
+            }
+
+            if (result[Permission.SEND_SMS] == GrantResult.PERMANENTLY_DENIED
+            ) {
+                showSnackBarOnPermissionError("Goto Settings Page", "Settings") {
+                    showSystemAppDetailsPage()
+                }
+            }
+
+        }
+    }
+    private fun showSnackBarOnPermissionError(
+        message: String,
+        textAction: String,
+        action: () -> Unit
+    ) {
+        Snackbar.make(requireView(), message, Snackbar.LENGTH_INDEFINITE)
+            .setAction(textAction) {
+                action()
+            }.setActionTextColor(Color.RED)
+            .show()
+    }
+
 }
